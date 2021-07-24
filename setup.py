43f882026d4347e2efa89e231df535bc7abadef8
Romain Beaumont <romain.rom1@gmail.com>
2021-07-24 14:59:52 +0200

    2.2.0

--- setup.py
+++ setup.py
@@ -2,20 +2,30 @@ import os
 
 import pkg_resources
 from setuptools import setup, find_packages
+from pathlib import Path
 
-setup(
-    name="clip-anytorch",
-    py_modules=["clip"],
-    version="2.1.0",
-    description="",
-    author="OpenAI",
-    packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
-    include_package_data=True,
-    extras_require={'dev': ['pytest']},
-)
+if __name__ == "__main__":
+
+    # Read description from README
+    with Path(Path(__file__).parent, "README.md").open(encoding="utf-8") as file:
+        long_description = file.read()
+
+    setup(
+        name="clip-anytorch",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        description=long_description.split("\n")[0],
+        url="https://github.com/rom1504/CLIP",
+        py_modules=["clip"],
+        version="2.2.0",
+        author="OpenAI",
+        packages=find_packages(exclude=["tests*"]),
+        install_requires=[
+            str(r)
+            for r in pkg_resources.parse_requirements(
+                open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+            )
+        ],
+        include_package_data=True,
+        extras_require={'dev': ['pytest']},
+    )
