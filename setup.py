be80ee64ec8c3e69b4c71862d81de062b0c3996d
Romain Beaumont <romain.rom1@gmail.com>
2021-12-13 17:59:00 +0100

    Release 2.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         description=long_description.split("\n")[0],
         url="https://github.com/rom1504/CLIP",
         py_modules=["clip"],
-        version="2.2.0",
+        version="2.2.1",
         author="OpenAI",
         packages=find_packages(exclude=["tests*"]),
         install_requires=[
